3438fb3febbdccce3a1f3e4abd6e746180e8e081
cfkwok <cfkwok@ufl.edu>
2016-04-25 17:18:29 -0400

    Refined bare scene (no env)

--- Assets/_Scripts/SOneMovement.cs
+++ Assets/_Scripts/SOneMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class SOneMovement : MonoBehaviour {
 	public GameObject player;
-    public float speed = 0.1f;
+    public float speed = 0.3f;
 
     //private CardboardHead head = null;
     private GameObject head;
@@ -39,6 +39,12 @@ public class SOneMovement : MonoBehaviour {
     float tiltAngle = 30.0F;
     private bool friendRotateBack;
     private bool question1;
+    private bool bullyWalkAway;
+    private GameObject victimObject;
+    private bool continueBully;
+    private int contBullyCount = 0;
+    private SceneFadeInOut sceneFader;
+    private bool goSceneTwo;
 
     // Use this for initialization
     void Start () {
@@ -47,6 +53,7 @@ public class SOneMovement : MonoBehaviour {
         panelGroup = GameObject.Find("QuestionMenu").GetComponent<CanvasGroup>();
         friendObject = GameObject.Find("Friend");
         bullyObject = GameObject.Find("Bully");
+        victimObject = GameObject.Find("Bullied");
         dest = new Vector3(player.transform.position.x, player.transform.position.y, -3.0f);
         textQuestion = GameObject.Find("Question").GetComponent<Text>();
         opt1 = GameObject.Find("Button1").GetComponent<Button>();
@@ -59,6 +66,9 @@ public class SOneMovement : MonoBehaviour {
 
         dialogFriend = GameObject.Find("DialogFriend").GetComponent<MeshRenderer>();
         dialogBully = GameObject.Find("DialogBully").GetComponent<MeshRenderer>();
+
+        sceneFader = GameObject.Find("Fader").GetComponent<SceneFadeInOut>();
+        // sceneFader.StartScene();
     }
     
     void Update()
@@ -70,7 +80,7 @@ public class SOneMovement : MonoBehaviour {
         rot.z = 270;
         rot.y += 95;
         dialogFriend.transform.rotation = Quaternion.Euler(rot);
-        dialogBully.transform.rotation = Quaternion.Euler(rot);
+        // dialogBully.transform.rotation = Quaternion.Euler(rot);
 
         float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
         float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
@@ -91,12 +101,37 @@ public class SOneMovement : MonoBehaviour {
             // bullyAnimator.SetInteger("Current State", 3);
             // Rotate the bully to face user
             bullyObject.transform.Rotate(Vector3.down * Time.deltaTime * 100.0f);
-            if (bullyObject.transform.eulerAngles.y <= 200)
+            if (bullyObject.transform.eulerAngles.y <= 185)
             {
                 talkedToBully = true;
                 bullyAnimator.SetInteger("Current State", 0);
-                changeQuestion("Do you want to interfere?", "Yes", "No");
-                // Make char glow to click on trigger
+
+                StartCoroutine(BullyWalk(5));
+
+
+            }
+        }
+        if (bullyWalkAway)
+        {
+            if (bullyObject.transform.position.z < victimObject.transform.position.z - 5)
+            {
+                Vector3 victimLocation = new Vector3(4.207713f, player.transform.position.y, -5.715645f);
+                player.transform.position = Vector3.Lerp(player.transform.position, victimLocation, speed * Time.deltaTime * 7);
+            }
+            else
+            {
+                Vector3 sidestep = new Vector3(3.18f, player.transform.position.y, -7.7f);
+                player.transform.position = Vector3.Lerp(player.transform.position, sidestep, speed * Time.deltaTime * 20);
+            }
+            
+            Vector3 tmp = new Vector3(bullyObject.transform.position.x, bullyObject.transform.position.y, -25f);
+            bullyObject.transform.position = Vector3.Lerp(bullyObject.transform.position, tmp, speed * Time.deltaTime);
+            if (bullyObject.transform.position.z <= -20f)
+            {
+                bullyWalkAway = false;
+                Destroy(bullyObject);
+
+                sceneFader.EndScene(1);
             }
         }
         if (friendRotate)
@@ -108,15 +143,21 @@ public class SOneMovement : MonoBehaviour {
                 friendRotate = false;
                 question2 = true;
                 friendAnimator.SetInteger("Current State", 2);
-                changeQuestion("Do you want to interfere?", "Yes", "No");
+                changeQuestion("Do you want to continue interfering?", "Yes", "No");
                 // Make char glow to click on trigger
                 dialogFriend.enabled = true;
             }
         }
         if (friendRotateBack)
         {
+            Vector3 newPos = friendObject.transform.position;
+            newPos.x = player.transform.position.x + 1f;
             friendObject.transform.Rotate(Vector3.up * Time.deltaTime * 100.0f);
+
+            friendObject.transform.position = Vector3.Lerp(friendObject.transform.position, newPos, speed * Time.deltaTime * 10f);
             friendAnimator.SetInteger("Current State", 1);
+            
+            // friendObject.transform.position = newPos;
             if (friendObject.transform.eulerAngles.y >= 350)
             {
                 friendRotateBack = false;
@@ -135,6 +176,7 @@ public class SOneMovement : MonoBehaviour {
                 }
             }
         }
+        /*
         if (destReached)
         {
             if (victimAnimator.GetCurrentAnimatorStateInfo(0).IsName("being_strangled"))
@@ -142,9 +184,11 @@ public class SOneMovement : MonoBehaviour {
                 if (victimAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
                 {
                     victimAnimator.SetInteger("Current State", 2);
+                    print("Hi");
                 }
             }
         }
+        */
         if (showMenu && !fadeMenu)
         {
             enableMenu();
@@ -169,8 +213,43 @@ public class SOneMovement : MonoBehaviour {
             }
             */
         }
-        
-           
+
+        if (continueBully)
+        {
+            bullyAnimator.SetInteger("Current State", 1);
+            victimAnimator.SetInteger("Current State", 1);
+            Vector3 watchPos = player.transform.position;
+            watchPos.z = friendObject.transform.position.z;
+            watchPos.x = friendObject.transform.position.x - 1f;
+            player.transform.position = Vector3.Lerp(player.transform.position, watchPos, speed * Time.deltaTime * 5);
+            if (victimAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+            {
+                bullyAnimator.Play("zombie_punching", -1, 0f);
+                victimAnimator.Play("getting_hit", -1, 0f);
+                contBullyCount++;
+            }
+            if (contBullyCount > 3)
+            {
+                continueBully = false;
+                bullyAnimator.SetInteger("Current State", 2);
+                StartCoroutine(ExecuteAfterTime(0.5f));
+            }
+
+        }
+        if (contBullyCount > 3 && bullyAnimator.GetCurrentAnimatorStateInfo(0).IsName("zombie_headbutt"))
+        {
+            if (bullyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+            {
+                bullyAnimator.Play("yelling", -1, 0f);
+                
+                goSceneTwo = true;
+            }
+        }
+        if (goSceneTwo)
+        {
+            sceneFader.EndScene(1);
+        }
+
     }
 
     void engageBully()
@@ -209,7 +288,13 @@ public class SOneMovement : MonoBehaviour {
         yield return new WaitForSeconds(time);
         victimDowned = true;        
     }
-    
+
+    IEnumerator BullyWalk(float time)
+    {
+        yield return new WaitForSeconds(time);
+        bullyWalkAway = true;
+    }
+
 
     void OnTriggerEnter(Collider other)
     {
@@ -280,6 +365,13 @@ public class SOneMovement : MonoBehaviour {
             print("Continue bully scene");
             fadeMenu = true;
             question1 = false;
+            continueBully = true;
+        }
+        if (question2 && !showMenu)
+        {
+            fadeMenu = true;
+            continueBully = true;
+            friendRotateBack = true;
         }
     }
 }
