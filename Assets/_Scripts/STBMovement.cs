d1bf8e4bab33c7d2933e947c330c5854b9d3b020
Craig Lu <Craig.Z.Lu@gmail.com>
2016-04-26 14:27:46 -0400

    added more to scene 2 bully encounter

--- Assets/_Scripts/STBMovement.cs
+++ Assets/_Scripts/STBMovement.cs
@@ -13,65 +13,116 @@ public class STBMovement : MonoBehaviour {
     private Animator bullyAnimator;
     private Button opt1;
     private Button opt2;
+    private Text decision;
+    private Quaternion headPos;
+    private float delayTime;
     private bool showMenu;
     private bool fadeMenu;
     private bool run;
     private bool bullyRun;
+    private bool bullyWalk;
     private bool question1;
     private bool question2;
+    private bool question3;
     public GameObject runDestination;
     void Start () {
-        goHere = this.transform.position;
+        goHere = player.transform.position;
         panelGroup = GameObject.Find("QuestionMenu").GetComponent<CanvasGroup>();
+        decision = panelGroup.transform.FindChild("Question").GetComponent<Text>();
         opt1 = GameObject.Find("Button1").GetComponent<Button>();
         opt2 = GameObject.Find("Button2").GetComponent<Button>();
         panelGroup.alpha = 0;
         bully = GameObject.Find("Bully");
         bullyAnimator = bully.GetComponent<Animator>();
+        bullyWalk = true;
+        fadeMenu = false;
     }
 
     void Update() {
-        player.transform.position = Vector3.Lerp(player.transform.position, goHere, speed * Time.deltaTime * 10);
+        player.transform.position = Vector3.MoveTowards(player.transform.position, goHere, speed * Time.deltaTime);
+        if (bullyWalk)
+        {
+            bully.transform.position = Vector3.MoveTowards(bully.transform.position, this.transform.position, speed * Time.deltaTime);
+            if(Vector3.Distance(this.transform.position, bully.transform.position) < .05f)
+            {
+                bullyAnimator.SetInteger("Current State", 1);
+                bullyWalk = false;
+            }
+        }
         if (Vector3.Distance(player.transform.position, goHere) < 1)
         {
             goHere = player.transform.position;
         }
-        if (showMenu)
+
+        if (showMenu && !fadeMenu)
         {
             enableMenu();
         }
-        if (fadeMenu)
+
+        if (fadeMenu && !showMenu)
         {
             disableMenu();
         }
+
         if (run)
         {
             player.GetComponentInChildren<CardboardHead>().trackRotation = false;
-            player.transform.Rotate(Vector3.down * Time.deltaTime * 100.0f);
+            player.transform.Rotate(Vector3.down * Time.deltaTime * 200.0f);
             if (player.transform.eulerAngles.y <= 180)
             {
+                delayTime = Time.time + 2f;
+                speed = 7;
                 player.GetComponentInChildren<CardboardHead>().trackRotation = true;
                 run = false;
                 goHere = runDestination.transform.position;
                 bullyRun = true;
             }
         }
-        if (bullyRun)
+
+        if (bullyRun && delayTime < Time.time)
         {
-            bully.transform.position = Vector3.Lerp(bully.transform.position, goHere, speed * Time.deltaTime * 10);
-            if(Vector3.Distance(bully.transform.position, goHere) < 2)
+            bully.transform.position = Vector3.MoveTowards(bully.transform.position, runDestination.transform.position, speed * Time.deltaTime * 1.3f);
+            bully.transform.LookAt(player.transform.FindChild("LookAtPivot").transform);
+            Debug.Log(Vector3.Distance(bully.transform.position, player.transform.position));
+            if (Vector3.Distance(bully.transform.position, player.transform.position) < 2.5)
             {
                 bullyRun = false;
+                goHere = player.transform.position;
+                bullyAnimator.SetInteger("Current State", 1);
+                decision.text = "Will you fight back or do nothing?";
+                opt1.GetComponentInChildren<Text>().text = "Fight back";
+                opt2.GetComponentInChildren<Text>().text = "Do nothing";
+                question2 = true; 
             }
         }
+        if (question2 && player.transform.eulerAngles.y >= 90)
+        {
+            player.transform.Rotate(Vector3.down * Time.deltaTime * -200.0f);
+            player.transform.FindChild("Head").Rotate(Vector3.down * Time.deltaTime * -200.0f); 
+            if (player.transform.eulerAngles.y <= 90)
+            {
+                showMenu = true;
+            }
+        }
+
+        if (bullyAnimator.GetCurrentAnimatorStateInfo(0).IsName("kick_to_the_groin"))
+        {
+            if (bullyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                bullyAnimator.SetInteger("Current State", 4);
+            }
+        }
+        Debug.Log(headPos);
     }
 
     void OnTriggerEnter()
     {
-        goHere = player.transform.position;
-        showMenu = true;
+        Debug.Log("Hello?");
+        if (!fadeMenu)
+        {
+            showMenu = true;
+        }
         question1 = true;
-        bullyAnimator.SetInteger("Current State", 1);
     }
     void enableMenu()
     {
@@ -89,10 +140,8 @@ public class STBMovement : MonoBehaviour {
         panelGroup.alpha -= 1f * Time.deltaTime;
         opt1.interactable = false;
         opt2.interactable = false;
-        if (panelGroup.alpha == 1f)
+        if (panelGroup.alpha == 0f)
         {
-            opt1.interactable = true;
-            opt2.interactable = true;
             fadeMenu = false;
         }
     }
@@ -111,6 +160,7 @@ public class STBMovement : MonoBehaviour {
         {
             question2 = false;
             fadeMenu = true;
+            bullyAnimator.SetInteger("Current State", 3);
         }
     }
     // Question X Choice 2
